8df16d9e46d99f9f4f4a532bdcee8d78b905a47f
Ivan Reyes <ivanwilf@hotmail.com>
2024-03-05 09:33:37 +0900

    Update comments in app.rb

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require 'yaml'
 require_relative "linebot"
 
-## Opens and loads both f the yaml files
+## Opens and loads both of the yaml files
 teams = YAML.load_file('./teams.yml')
 current_team = teams[0]
 members = YAML.load_file('./members.yml')[current_team]
 message = "This week, it's #{current_team} team's turn for cleaning! (#{members.join(', ')}) がんばろうー！"
 
-## Open and rewrites the yaml file
+## Open and rewrite the teams yaml file.
 teams.insert(-1, teams.delete_at(0))
 File.open("./teams.yml", 'w') { |f| YAML.dump(teams, f) }
 
